98d35cb172726f35cd4147ad19e35a1a7aa6c687
ll-alamin-ll <ll.alamin.ll@gmail.com>
2019-12-09 14:51:41 +0600

    finished

--- js/custom.js
+++ js/custom.js
@@ -1,164 +1,282 @@
-/*================================
-    Selector Banner part
-=================================*/
-(function() {
-  //pick up address
-  var addressList = {
-    1: "Heathrow Terminal 2 TW6 1RR",
-    2: "Heathrow Terminal 3 TW6 1QG",
-    3: "Heathrow Terminal 4 TW6 3XA",
-    4: "Heathrow Terminal 5 TW6 2GA",
-    5: "City Airport E16 2PX",
-    6: "Stansted Airport CM24 1RW",
-    7: "Gatwick South RH6 0NP",
-    8: "Gatwick North RH6 0RD",
-    9: "Southend Airport SS2 6YF",
-    10: "Luton Airport LU2 9LY"
-  };
+/*==================================
+     bar scrolling
+====================================*/
 
-  $("#select-box-preselect1").bselect({
-    data: addressList,
-    search: false,
-    width: "100%",
-    defaultText: "Pickup Address"
-  });
+(function () {
+    $(window).scroll(function () {
+        var scrolling = $(document).scrollTop();
 
-  //Destination address
-  var destination = {
-    1: "Heathrow Terminal 2 TW6 1RR",
-    2: "Heathrow Terminal 3 TW6 1QG",
-    3: "Heathrow Terminal 4 TW6 3XA",
-    4: "Heathrow Terminal 5 TW6 2GA",
-    5: "City Airport E16 2PX",
-    6: "Stansted Airport CM24 1RW",
-    7: "Gatwick South RH6 0NP",
-    8: "Gatwick North RH6 0RD",
-    9: "Southend Airport SS2 6YF",
-    10: "Luton Airport LU2 9LY"
-  };
-
-  $("#select-box-preselect2").bselect({
-    data: destination,
-    search: false,
-    width: "100%",
-    defaultText: "Destination Address"
-  });
-
-  //Person
-  var person = {
-    1: "Person 01",
-    2: "Person 02",
-    3: "Person 03",
-    4: "Person 04",
-    5: "Person 05",
-    6: "Person 06",
-    7: "Person 07",
-    8: "Person 08"
-  };
-
-  $("#select-box-preselectPerson").bselect({
-    data: person,
-    search: false,
-    selected: 2,
-    width: "100%",
-    defaultText: "Select..."
-  });
-
-  //luggage
-  var luggage = {
-    1: "Luggage 01",
-    2: "Luggage 02",
-    3: "Luggage 03",
-    4: "Luggage 04",
-    5: "Luggage 05",
-    6: "Luggage 06",
-    7: "Luggage 07",
-    8: "Luggage 08"
-  };
-
-  $("#select-box-preselectLuggage").bselect({
-    data: luggage,
-    search: false,
-    selected: 2,
-    width: "100%",
-    defaultText: "Select..."
-  });
+            
+        if (scrolling > 48) {
+            $(".bar_line").css({
+                background: "#1fbfee",
+                height: scrolling * 0.09 * 9
+            });
+        } else {
+            $(".bar_line").css({
+                background: "#1fbfee",
+                height: "18%"
+            });
+        }
+    });
 })();
+
+
+/*================================
+    datePicker
+=================================*/
+
+(function () {
+    
+         
+         var datepickerBtn = $("#date_picker_input")
+         
+            datepickerBtn.click(function(){
+             $('.datePicker').fadeIn();
+             })
+    
+            
+            function logEvent(type, date) {
+                $("<div class='log__entry'/>").hide().html("<strong>"+type + "</strong>: "+date).prependTo($('#eventlog')).show(200);
+            }
+            $('#clearlog').click(function() {
+                $('#eventlog').html('');
+            });
+
+ 
+        
+            $('#demo2').datetimepicker({
+                date: new Date(),
+                viewMode: 'YMD',
+                onDateChange: function(){
+                    $('#date-text2').text(this.getText());
+                  
+                }
+            });
+            
+    
+
+            
+   
+    
+    
+    
+
+     var hourBox = document.querySelector("#hour_box");
+     var minBox = document.querySelector("#minBox")
+    
+     var h_up = document.querySelector("#h_up");
+     var h_down = document.querySelector("#h_down");
+    
+     var m_up = document.querySelector("#m_up");
+     var m_down = document.querySelector("#m_down");
+
+    dateBase = {
+        hour: 0,
+        minute: 0,
+    }
+
+    function displayDOM() {
+
+        hourBox.innerHTML = dateBase.hour;
+        minBox.innerHTML  = dateBase.minute;
+
+    }
+
+   
+
+    h_up.addEventListener("click", function () {
+
+        if (dateBase.hour < 23) {
+            
+            dateBase.hour++;
+            displayDOM()
+        }
+    })
+    
+    h_down.addEventListener("click", function () {
+
+        if (dateBase.hour > 0) {
+            
+            dateBase.hour;
+            displayDOM()
+        }
+    })
+    
+    
+    
+    m_up.addEventListener("click", function () {
+
+        if (dateBase.minute < 60) {
+            
+            dateBase.minute = dateBase.minute + 5;
+            displayDOM()
+        }
+    })
+    
+    m_down.addEventListener("click", function () {
+
+        if (dateBase.minute > 0) {
+            
+            dateBase.minute = dateBase.minute - 5;
+            displayDOM()
+        }
+    })
+    
+    $(".title").click(function(){
+       
+        $(".ok").css({
+            'opacity':'1',
+            'visibility':'visible',
+        });
+        
+        
+        $(".ok").click(function(){
+            $(".ok").css({
+            'opacity':'0',
+            'visibility':'hidden',
+        });
+        })
+        
+    });
+    
+    $("#ok_btn").click(function(){
+        
+        var day = $(".selected").text();
+        var mon = $(".title").text();
+        
+        var date = day+', '+mon+', '+dateBase.hour+':'+dateBase.minute;
+        
+        
+        $("#date_picker_input").val(date)
+  
+        $('.datePicker').fadeOut();
+          
+    })
+    
+    $("#cancel_btn").click(function(){
+        $('.datePicker').fadeOut();
+    })
+    
+    
+    $("#asap_btn").click(function(){
+        
+         $("#date_picker_input").val("ASAP")
+          $('.datePicker').fadeOut();
+    })
+    
+    
+    
+    
+    
+
+
+
+
+})();
+
+
 
 /*==================================
   video plugin -venobox
 ====================================*/
-(function() {
-  $(document).ready(function() {
-    $(".venobox").venobox();
-  });
+(function () {
+    $(document).ready(function () {
+        $(".venobox").venobox();
+    });
 })();
 
 /*==================================
   event binding 
 ====================================*/
-(function() {
-  $("#menu_btn").click(function() {
-    popUp(true);
-  });
+(function () {
+    $("#menu_btn").click(function () {
+        popUp(true);
+    });
+    
+    $("#signIn_btn").click(function () {
+        shadowFun(true)
+    });
+    
 
-  $("#close_menu").click(function() {
-    popUp(false);
-  });
+    $("#close_menu").click(function () {
+        popUp(false);
+    });
+    
+    $(".close_sup").click(function(){
+        shadowFun(false);
+    })
 
-  //this function to toggle popup
-  function popUp(act) {
-    if (act === true) {
-      $(".sideMenu").css({
-        display: "block"
-      });
+    //this function to toggle popup
+    function popUp(act) {
+        if (act === true) {
+            $(".sideMenu").css({
+                display: "block"
+            });
 
-      $("body").css({
-        overflow: "hidden"
-      });
+            $("body").css({
+                overflow: "hidden"
+            });
 
-      $(".menu_bar").animate({
-        right: "0"
-      });
-    } else if (act === false) {
-      $(".menu_bar").animate(
-        {
-          right: "-300px"
-        },
-        function() {
-          $(".sideMenu").css({
-            display: "none"
-          });
+            $(".menu_bar").animate({
+                right: "0"
+            });
+        } else if (act === false) {
+            $(".menu_bar").animate({
+                    right: "-300px"
+                },
+                function () {
+                    $(".sideMenu").css({
+                        display: "none"
+                    });
+                }
+            );
+
+            $("body").css({
+                overflow: "auto"
+            });
         }
-      );
-
-      $("body").css({
-        overflow: "auto"
-      });
     }
-  }
+    
+    
+    function shadowFun(act){
+        if(act === true){
+            $(".sideMenu").css({
+                display: "block"
+            });
+            
+            $(".sup").animate({
+                'opacity': 1,
+            })
+            
+            $("body").css({
+                overflow: "hidden"
+            });
+            
+        }else if(act === false){
+            
+            
+            $(".sup").animate({
+                
+                'opacity': 0,
+                
+            }, function(){
+                
+                 $(".sideMenu").css({
+                    display: "none"
+                  });
+                
+            
+                
+            })
+            
+               $("body").css({
+                overflow: "auto"
+            }); 
+            
+            
+            
+        }
+    }
 })();
 
-/*==================================
-     bar scrolling
-====================================*/
-
-(function() {
-  $(window).scroll(function() {
-    var scrolling = $(document).scrollTop();
-
-    console.log(scrolling);
-
-    if (scrolling > 48) {
-      $(".bar_line").css({
-        background: "#1fbfee",
-        height: scrolling * 0.09 * 9
-      });
-    } else {
-      $(".bar_line").css({
-        background: "#1fbfee",
-        height: "18%"
-      });
-    }
-  });
-})();
